14c480a6e038afe7dc1ee4f10d2d86944c035f5a
Juan Sebastian Soscue Portes <104285353+jsoscue@users.noreply.github.com>
2024-11-09 01:45:55 -0500

    Update Salir.js

--- gps-tracker/Admin/Firabase/Salir.js
+++ gps-tracker/Admin/Firabase/Salir.js
@@ -1,6 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,signOut } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
 import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyD0Im_ROxNZcWaBhj9w4Ncrh7MOELnWZGE",
+  authDomain: "naviway-database.firebaseapp.com",
+  projectId: "naviway-database",
+  storageBucket: "naviway-database.appspot.com",
+  messagingSenderId: "35224588554",
+  appId: "1:35224588554:web:8a44a81de4c5820a2c2af3",
+  measurementId: "G-MTB3GZZZ9N"
+};
 export async function cerrarsesion() {
   signOut(auth)
     .then(() => {
