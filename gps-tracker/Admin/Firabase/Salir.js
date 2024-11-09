84ace1bfb8e7a61ac66860c0db1c2f9a8fdb8a44
Juan Sebastian Soscue Portes <104285353+jsoscue@users.noreply.github.com>
2024-11-09 01:25:11 -0500

    Update Salir.js

--- gps-tracker/Admin/Firabase/Salir.js
+++ gps-tracker/Admin/Firabase/Salir.js
@@ -6,10 +6,11 @@ export async function cerrarsesion() {
     .then(() => {
       localStorage.removeItem('loggedIn');
       localStorage.removeItem('userPhotoURL');
-      window.location.href = '../index.html'; // Redirect to home page
+      window.location.href = '../../index.html'; // Redirect to home page
     })
     .catch((error) => {
       console.error('Error signing out:', error);
       alert('Error signing out. Please try again.');
     });
 }
+window.cerrar = cerrar;
