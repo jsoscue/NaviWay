37635fa2eeaa0b03ae8f5a3af41ad85a01360e0c
Juan Sebastian Soscue Portes <104285353+jsoscue@users.noreply.github.com>
2024-11-18 21:22:12 -0500

    Update rutas.js

--- gps-tracker/Admin/rutas/rutas.js
+++ gps-tracker/Admin/rutas/rutas.js
@@ -1,6 +1,11 @@
 document.getElementById('route-form').addEventListener('submit', function(event) {
+    // Previene el comportamiento por defecto del formulario (recargar la página)
     event.preventDefault();
+
+    // Obtiene los valores de los campos de inicio y destino del formulario
     const start = document.getElementById('start').value;
     const end = document.getElementById('end').value;
+
+    // Redirige al usuario a la página del mapa con los parámetros de inicio y destino en la URL
     window.location.href = `../map.html?start=${start}&end=${end}`;
 });
